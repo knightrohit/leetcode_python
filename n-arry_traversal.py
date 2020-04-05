2eb1432330e57d14f183ac4d9985ee517154d747
Rohit <knight.rohit@gmail.com>
2020-04-05 17:11:27 -0500

    PreOrder traversal

---
+++ n-arry_traversal.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, val = None, childern = []):
+        self.val = val
+        self.children = children
+
+class PreOrder:
+    def preorder(self, root: 'Node') -> List[int]:
+        
+        if not root:
+            return []
+
+        stack, out = [root], []  
+        while stack:
+            node = stack.pop()
+            out.append(node.val)
+            if node.children:
+                stack.extend(node.children[::-1])
+                
+        return out
