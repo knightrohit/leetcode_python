7928ab7444d425f17e1552ff0ea53c85da05269a
Rohit <knight.rohit@gmail.com>
2020-04-07 15:49:59 -0500

    using bfs approach

--- n-ary-tree-depth.py
+++ n-ary-tree-depth.py
@@ -4,6 +4,7 @@ class Node:
         self.val = val
         self.children = children
 
+# Calculate max depth using dfs
 class Solution:
     def maxDepth(self, root: 'Node', depth = 1) -> int:
         
@@ -17,3 +18,30 @@ class Solution:
         for node in root.children:
             height.append(self.maxDepth(node, depth+1))
         return max(height)
+
+
+# Calculate max depth using bfs
+from collections import deque
+class BFSSolution:
+    def maxDepth(self, root: 'Node') -> int:
+        
+        if root == None:
+            return 0
+               
+        if not root.children and root:
+            return 1
+        
+        depth = 0
+        queue = deque() 
+        queue.append(root)
+        
+        while queue:
+            temp = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                temp.extend(node.children)
+
+            queue.extend(temp)
+            depth +=1
+        
+        return depth
