4c92ffbcefb9e754a6e9a4e1dc5e8b9c0127c502
Rohit <knight.rohit@gmail.com>
2020-05-01 16:51:00 -0500

    used dictionary to make a little clean

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -26,3 +26,39 @@ class Solution:
             stack.append(ch)
             
         return len(stack) == 0
+
+
+####################################
+# Little clean code using dictionary
+####################################
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        if s == None:
+            return None
+        
+        if s == '':
+            return True
+        
+        if len(s) == 1:
+            return False
+        
+        stack = []
+        mappings = {')':'(', ']':'[', '}':'{'}
+        
+        for ch in s:
+            if not stack:
+                stack.append(ch)
+                continue
+                
+            if ch in mappings:
+                prev_item = stack[-1]
+
+                if mappings[ch] == prev_item:
+                    stack.pop()
+                    continue
+            
+            stack.append(ch)
+            
+        return not stack
+            
